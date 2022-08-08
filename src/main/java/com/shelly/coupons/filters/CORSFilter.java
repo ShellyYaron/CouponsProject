687e2addbb815b9112b14f3213f8469ac2875c10
Shelly Yaron <shellyb2911@gmaial.com>
2022-08-08 19:50:18 +0300

    Fixed pom

--- src/main/java/com/shelly/coupons/filters/CORSFilter.java
+++ src/main/java/com/shelly/coupons/filters/CORSFilter.java
@@ -1,13 +1,19 @@
-package com.shelly.coupons.filters;/*
-package com.shelly.com.shelly.coupons.filters;
+package com.eitan.couponsproject.filters;
 
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+ 
 @Component
 public class CORSFilter implements Filter {
  
@@ -22,12 +28,12 @@ public class CORSFilter implements Filter {
  
         HttpServletRequest request = (HttpServletRequest) servletRequest;
  
-        // Authorize (allow) all domains to consume the content  		http://localhost:4200
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin","*");
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin","http://localhost:4200");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers","password,authorization, Origin, Accept, x-auth-token, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers","authorization, Origin, Accept, x-auth-token, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-        //HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
  
         // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
         if (request.getMethod().equals("OPTIONS")) {
@@ -41,4 +47,4 @@ public class CORSFilter implements Filter {
     public void init(FilterConfig fConfig) throws ServletException {
     }
  
-}*/
+}
